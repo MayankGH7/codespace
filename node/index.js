c204b116e90f2d70bc2e4b436e9054ced2708393
MayankGH7 <studioarjun@gmail.com>
2023-10-02 20:12:50 +0530

    change

--- node/index.js
+++ node/index.js
@@ -1,6 +1,6 @@
+const fs = require('fs')
 console.log('Codespace created successfully...')
 console.log('Hello, user')
-import fs from 'fs'
 fs.readFile('./new.txt',(err,data)=>{
     console.log(data)
 })
