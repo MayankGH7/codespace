52f6d85637e8b74ea3378c0a8b34ec857434bafe
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2023-10-02 15:46:19 +0000

    c

--- node/index.js
+++ node/index.js
@@ -1,9 +1,10 @@
 import fs from 'fs';
 
-fs.readFile('./node/new.txt', 'utf8', (err, data) => {
+fs.readFile('./new.txt', 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading the file:', err);
     return;
   }
   console.log(data);
 });
+
